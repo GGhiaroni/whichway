851e7d53a95aea0d2101294cc97e998875885e8b
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-19 21:52:55 -0300

    avancando com o layout da home e a section features

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 
@@ -5,9 +7,12 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-brand-cream">
       <Header />
-      <Hero />
+      <main className="flex-1 pt-16">
+        <Hero />
 
-      <main className="flex-1 pt-16"></main>
+        <Features />
+      </main>
+      <Footer />
     </div>
   );
 }
