c72aac7c37d659ca58a42e450c2710e1558e1696
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-23 19:20:27 -0300

    comecando a melhorar a pagina do saiba-mais

--- src/app/(public)/saiba-mais/page.tsx
+++ src/app/(public)/saiba-mais/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
-import { TrendingUp } from "lucide-react";
+import { MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -48,7 +48,7 @@ export default async function SaibaMais(props: PageProps) {
 
   return (
     <main>
-      <div className="relative h-[45vh] min-[390px]:h-[50vh] sm:h-[55vh] min-[1000px]:h-[75vh] lg:h-[70vh] w-full overflow-hidden">
+      <div className="relative h-[55vh] min-[390px]:h-[50vh] sm:h-[55vh] min-[1000px]:h-[75vh] lg:h-[70vh] w-full overflow-hidden">
         <Image
           src={destination.imageUrl}
           alt={destination.city}
@@ -59,41 +59,64 @@ export default async function SaibaMais(props: PageProps) {
 
         <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-transparent" />
 
-        <div className="absolute top-0 left-0 w-full z-10 pt-6 md:pt-12">
-          <div className="container mx-auto px-4 sm:px-8">
+        <div className="absolute top-0 left-0 w-full z-10 pt-40 md:pt-12">
+          <div className="container mx-auto pt-4 px-4 sm:px-8">
             <div className="inline-flex items-center rounded-full shadow-sm px-3 py-1 gap-2 bg-brand-dark ">
-              <TrendingUp className="h-4 w-4 text-brand-cream" />
               <h1 className="font-sans text-xs font-bold tracking-wider uppercase text-brand-cream">
-                Destinos em alta 🔥
+                Destino popular 🔥
               </h1>
             </div>
           </div>
 
-          <div className="flex flex-col px-4 sm:px-8 mx-auto container pt-14 min-[390px]:pt-40 min-[400px]:pt-35 sm:pt-36 md:pt-38">
+          <div className="flex flex-col gap-8 px-4 sm:px-8 mx-auto container pt-4 min-[390px]:pt-40 min-[400px]:pt-35 sm:pt-36 md:pt-38">
             <h2 className="text-4xl min-[390px]:text-5xl sm:text-7xl md:text-7xl uppercase font-sans text-white font-bold">
-              {destination.city},
+              {destination.city}
             </h2>
-            <h3 className="text-2xl min-[390px]:text-3xl sm:text-5xl max-w-[220px] min-[390px]:max-w-[250px] md:max-w-[400px] uppercase font-sans text-gray-400 font-bold">
-              {destination.country}
-            </h3>
 
-            <p className="hidden min-[400px]:block md:block mt-6 mb-2 font-bold min-[400px]:text-sm md:text-lg text-gray-200 max-w-xl leading-relaxed drop-shadow-md border-l-2 border-brand-primary pl-4">
-              {destination.description}
-            </p>
+            <div className="flex items-center gap-2 text-gray-400">
+              <MapPin className="h-5 w-5" />
+              <h3 className="text-xl min-[390px]:text-3xl sm:text-5xl max-w-[220px] min-[390px]:max-w-[250px] md:max-w-[400px] font-sans">
+                {destination.country}
+              </h3>
+            </div>
 
-            <div className="pt-4 flex gap-4">
+            <div className="flex flex-col gap-4">
               <Link href={`/criar-roteiro?destino=${destination.city}`}>
                 <Button className="h-6 p-4 bg-brand-dark hover:bg-brand-primary text-white font-bold uppercase font-sans text-xs rounded-full  shadow-[0_0_20px_rgba(0,0,0,0.3)]">
-                  Gerar roteiro
+                  REVIEWS & ESTRELAS AQUI
                 </Button>
               </Link>
-              <Link href={`/saiba-mais?destino=${destination.city}`}>
-                <Button className="h-6 p-4 bg-brand-primary hover:bg-brand-light text-brand-cream font-bold uppercase font-sans text-xs rounded-full  shadow-[0_0_20px_rgba(0,0,0,0.3)]">
-                  Saiba mais
+              <Link href={`/criar-roteiro?destino=${destination.city}`}>
+                <Button className="h-6 p-4 bg-brand-dark hover:bg-brand-primary text-white font-bold uppercase font-sans text-xs rounded-full  shadow-[0_0_20px_rgba(0,0,0,0.3)]">
+                  CRIE SEU ROTEIRO
                 </Button>
               </Link>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 mt-4">
+        <h3 className="text-2xl text-brand-dark">Sobre esse destino</h3>
+
+        <div className="rounded-3xl bg-brand-dark px-6 py-6 mt-4 shadow-2xl">
+          <p className="text-lg text-brand-cream">{destination.description}</p>
+        </div>
+
+        <div className="mt-6">
+          <h2 className="text-brand-primary text-xl mb-4">✨ Highlights</h2>
+          {destination.highlights ? (
+            destination.highlights.split(",").map((h, index) => (
+              <span
+                key={index}
+                className="px-4 py-2 rounded-full bg-brand-primary/10 border border-brand-primary/20 text-brand-dark font-medium text-sm capitalize"
+              >
+                {h.trim()}
+              </span>
+            ))
+          ) : (
+            <p className="text-gray-500 text-sm">Nenhum destaque informado.</p>
+          )}
         </div>
       </div>
     </main>
