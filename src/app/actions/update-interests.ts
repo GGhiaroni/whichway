dd7e0ac1edc1cc09f7b78ae34701b65baba751df
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-29 15:03:17 -0300

    criando a action update interests

---
+++ src/app/actions/update-interests.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export default async function updateInterests(newInterests: string[]) {
+  try {
+    const clerkUser = await currentUser();
+
+    if (!clerkUser || !clerkUser.emailAddresses[0]) {
+      return { success: false, error: "Usuário não autenticado." };
+    }
+
+    const email = clerkUser.emailAddresses[0].emailAddress;
+
+    await prisma.user.update({
+      where: { email },
+      data: {
+        interests: newInterests,
+      },
+    });
+
+    revalidatePath("/meu-perfil");
+
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao atualizar interesses:", error);
+    return { success: false, error: "Falha ao salvar interesses." };
+  }
+}
