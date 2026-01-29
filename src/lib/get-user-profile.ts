3092f03e5d016c3fb42824e32eeabf004587fa55
GGhiaroni <gghiaronitiziano@gmail.com>
2026-01-29 14:01:21 -0300

    criando a query responsável por buscar os dados do cliente no banco de dados: places visited, wishlists e roteiros criados

---
+++ src/lib/get-user-profile.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { PlaceStatus } from "@prisma/client";
+
+export default async function getUserProfile() {
+  try {
+    const clerkUser = await currentUser();
+
+    if (!clerkUser || !clerkUser.emailAddresses[0]) {
+      return { success: false, error: "Usuário não autenticado." };
+    }
+
+    const email = clerkUser.emailAddresses[0].emailAddress;
+
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        trips: {
+          orderBy: { createdAt: "desc" },
+        },
+
+        places: {
+          orderBy: { createdAt: "desc" },
+        },
+      },
+    });
+
+    if (!dbUser) {
+      return { success: false, error: "Usuário não encontrado no banco." };
+    }
+
+    const visitedPlaces = dbUser.places.filter(
+      (p) => p.status === PlaceStatus.VISITED,
+    );
+
+    const whishlistPlaces = dbUser.places.filter(
+      (p) => p.status === PlaceStatus.WISHLIST,
+    );
+
+    return {
+      success: true,
+      user: {
+        ...dbUser,
+        visitedPlaces,
+        whishlistPlaces,
+      },
+    };
+  } catch (error) {
+    console.error("Erro ao buscar prefil:", error);
+    return { success: false, error: "Erro interno ao buscar dados." };
+  }
+}
